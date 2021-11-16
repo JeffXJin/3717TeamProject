fac64cfad0eb80e206edca7b416e46ca721459f9
khion <nick_gayem@yahoo.com>
2021-11-15 21:32:32 -0800

    Added credit form activity and modify Contribute fragment UI

--- app/src/main/java/ca/bcit/androidProject/ContributeFragment.java
+++ app/src/main/java/ca/bcit/androidProject/ContributeFragment.java
@@ -1,5 +1,7 @@
 package ca.bcit.androidProject;
 
+import android.app.AlertDialog;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,13 +9,35 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.braintreepayments.cardform.view.CardForm;
 
 public class ContributeFragment extends Fragment {
+
+    CardForm cardForm;
+    Button buy;
+    AlertDialog.Builder alertBuilder;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View view;
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_contribute, container, false);
+        view = inflater.inflate(R.layout.fragment_contribute, container, false);
+        Button button = view.findViewById(R.id.btnDonate);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), CreditFormActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+        return view;
     }
+
 }
---
+++ app/src/main/java/ca/bcit/androidProject/CreditFormActivity.java
@@ -0,0 +1,95 @@
+package ca.bcit.androidProject;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.InputType;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.braintreepayments.cardform.view.CardForm;
+
+public class CreditFormActivity extends AppCompatActivity {
+
+
+    CardForm cardForm;
+    Button donate;
+    AlertDialog.Builder alertBuilder;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_credit_form);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        // add back arrow to toolbar
+        if (getSupportActionBar() != null){
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setDisplayShowHomeEnabled(true);
+        }
+
+
+        cardForm = findViewById(R.id.card_form);
+        donate = findViewById(R.id.btnBuy);
+        cardForm.cardRequired(true)
+                .expirationRequired(true)
+                .cvvRequired(true)
+                .postalCodeRequired(true)
+                .mobileNumberRequired(true)
+                .mobileNumberExplanation("SMS is required on this number")
+                .setup(CreditFormActivity.this);
+        cardForm.getCvvEditText().setInputType(InputType.TYPE_CLASS_NUMBER | InputType.TYPE_NUMBER_VARIATION_PASSWORD);
+        donate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (cardForm.isValid()) {
+                    alertBuilder = new AlertDialog.Builder(CreditFormActivity.this);
+                    alertBuilder.setTitle("Confirm before purchase");
+                    alertBuilder.setMessage("Card number: " + cardForm.getCardNumber() + "\n" +
+                            "Card expiry date: " + cardForm.getExpirationDateEditText().getText().toString() + "\n" +
+                            "Card CVV: " + cardForm.getCvv() + "\n" +
+                            "Postal code: " + cardForm.getPostalCode() + "\n" +
+                            "Phone number: " + cardForm.getMobileNumber());
+                    alertBuilder.setPositiveButton("Confirm", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            dialogInterface.dismiss();
+                            Toast.makeText(CreditFormActivity.this, "Thank you for your donation!", Toast.LENGTH_LONG).show();
+                            Intent intent = new Intent(CreditFormActivity.this, ContributeFragment.class);
+                            startActivity(intent);
+                        }
+                    });
+                    alertBuilder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            dialogInterface.dismiss();
+                        }
+                    });
+                    AlertDialog alertDialog = alertBuilder.create();
+                    alertDialog.show();
+
+                } else {
+                    Toast.makeText(CreditFormActivity.this, "Please complete the form", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+
+
+    /**
+     * This will display back button and allow user to go back to previous activity.
+     * @return true
+     */
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
+    }
+}
